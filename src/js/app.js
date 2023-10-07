53ea94f1eecb228b8db3ab62015f3552c1f0f266
Denis Vorobev <Kurtcobaine192@outlook.com>
2023-10-07 17:32:59 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
-import './dom';
+/* eslint-disable no-unused-vars */
+import Board from './Board';
+
+const fields = Array.from(document.querySelectorAll('.field'));
+
+const board = new Board(fields);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const interval = setInterval(() => {
+    board.deleteActiveClass();
+    board.addActiveClass();
+  }, 1000);
+});
